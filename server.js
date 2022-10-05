cc3c79cd48afb64857a37812995cf4420965d45b
Abdullah Adel <abdullahadel@kiitos-tech.com>
2022-10-05 10:41:31 +0300

    uses mongo atlas for storage

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const todoRoute = require("./routes/todos");
 const app = express();
 
 // connecting to mongoDB database and applying listeners on connection state
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  auth: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error)).on("open", () =>
   console.log("Connected to database")
@@ -14,7 +20,6 @@ db.on("error", (error) => console.error(error)).on("open", () =>
 // app.use <-- middleware
 // express.json() <-- parses json and only looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
-
 
 app.use("/todo", todoRoute);
 
